dbbd8905cb6df1d3340ce1a3c92d02f3a6ecc51d
Alon Nachshon <alonn@wix.com>
2017-03-11 16:14:37 +0200

    client serve - fetch link from the db

--- src/api/routes/pages.js
+++ src/api/routes/pages.js
@@ -1,13 +1,19 @@
 import express from '../express';
 import routeHandler from '../routeHandler';
+import {getLatestAssetLink} from '../../application/versionableAssets';
 
 express.get('/', serveClient());
 express.get('/react/*', serveClient());
 
 function serveClient() {
     return routeHandler(async (request, response) => {
+        const test = process.env.ENV === 'test';
+        const link = await getLatestAssetLink('resonators-client');
+
         response.status(200);
-        response.render('../pages/index');
+        response.render('../pages/index', {
+            link
+        });
     }, {
         enforceLogin: false
     });
