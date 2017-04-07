ce688c1e0c7c3099559dea22f1c68ff83c3a05c0
Alon Nachshon <alonn@wix.com>
2017-04-07 17:51:07 +0300

    index: output the env in the startup log

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ startEmailSchedulingLoop();
 api.set('port', (process.env.PORT || 8080));
 
 api.listen(api.get('port'), '0.0.0.0', function() {
-    console.log('Node app is running on port', api.get('port'));
+    console.log('Node app is running on port', api.get('port'), '| env:', process.env.ENV);
 });
