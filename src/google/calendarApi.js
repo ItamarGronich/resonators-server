02c62b7677a7b79e76dd485f641565047eb856c2
Alon Nachshon <alonn@wix.com>
2017-07-01 16:53:08 +0300

    google calendar api

---
+++ src/google/calendarApi.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import google from 'googleapis';
+import dispatch from './dispatcher';
+
+const calendar = google.calendar('v3');
+
+//{items}
+export function listCalendars(tokens) {
+    return dispatch(calendar.calendarList.list, tokens);
+}
+
+//{kind, etag, id, summary}
+export function createCalendar(tokens, params) {
+    return dispatch(calendar.calendars.insert, tokens, params);
+}
+
+//void
+export function removeCalendar(tokens, calendarId) {
+    return dispatch(calendar.calendarList.delete, tokens, {
+        calendarId
+    });
+}
+
+export function createEvent(args) {
+    return; 
+}
