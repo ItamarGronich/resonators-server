44a206a27baca84c6793ca1074d624d1e6c65357
Alon Nachshon <alonn@wix.com>
2017-08-12 16:17:28 +0300

    fix

--- src/index.http.js
+++ src/index.http.js
@@ -2,6 +2,6 @@ import http from 'http';
 import cfg from './cfg';
 
 http.createServer((req, res) => {
-    res.redirect(cfg.host);
+    res.writeHead(301, {'Location': cfg.host});
     res.end();
 }).listen(process.env.PORT || 80);
