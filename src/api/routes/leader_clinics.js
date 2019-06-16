a2506722586ca1c09f85966d70e477d74aaeb477
Ramaprasad <ac4rama18@gmail.com>
2019-06-16 21:29:45 +0530

    Add leader to clinic API Changes for Admin level functionality

--- src/api/routes/leader_clinics.js
+++ src/api/routes/leader_clinics.js
@@ -2,18 +2,34 @@ import express from '../express';
 import routeHandler from '../routeHandler';
 import {
     getLeaderClinics,
+    getLeaderClinicsIncludingSecondary,
     getLeaderClinicsCriteria,
     addQuestionToClinic,
-    updateQuestion
+    updateQuestion,
+    UpsertLeaderClinics,
+    UpdateCurrentClinicId,
+    addLeaderToClinic
 } from '../../application/leaderClinics';
 
 express.get('/api/leader_clinics', routeHandler(async (request, response) => {
-    const {user} = request.appSession;
-
-    const clinics = await getLeaderClinics(user.id);
-
+    const {leader} = request.appSession;
+    const clinics = await getLeaderClinicsIncludingSecondary(leader.id);
     response.status(200);
     response.json(clinics);
+}));
+
+express.get('/api/upsert_leader_clinics', routeHandler(async (request, response) => {
+    const {leader} = request.appSession;
+    await UpsertLeaderClinics();
+    response.status(200);
+    response.json(leader);
+}));
+
+express.get('/api/updateCurrentClinicId', routeHandler(async (request, response) => {
+    const {leader} = request.appSession;
+    await UpdateCurrentClinicId();
+    response.status(200);
+    response.json(leader);
 }));
 
 express.get('/api/leader_clinics/:clinicId/criteria\.:ext?', routeHandler(async (request, response) => {
@@ -31,6 +47,13 @@ express.get('/api/leader_clinics/:clinicId/criteria\.:ext?', routeHandler(async 
         response.status(200);
 
     response.json(questions);
+}));
+
+express.post('/api/leader_clinics\.:ext?', routeHandler(async (request, response) => {
+    const {leader} = request.appSession;
+    var leaderClinic = request.body.email;
+    await addLeaderToClinic(leaderClinic);
+    response.status(201);
 }));
 
 express.post('/api/leader_clinics/:clinicId/criteria\.:ext?', routeHandler(async (request, response) => {
