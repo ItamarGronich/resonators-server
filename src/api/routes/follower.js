74f5b67a549cf1602d9bb18e3dc6000a0bce3c41
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-07-22 18:12:30 +0300

    rewrote everything with a single query that also selects answer data

--- src/api/routes/follower.js
+++ src/api/routes/follower.js
@@ -2,14 +2,53 @@ import express from '../express';
 
 import routeHandler from '../routeHandler';
 import { getResonators } from '../../application/resonators'
+import {
+    leaders,
+    resonators,
+    questions,
+    resonator_attachments,
+    resonator_questions,
+    sent_resonators,
+    resonator_answers
+} from '../../db/sequelize/models'
 
 
 express.get('/api/follower/resonators', routeHandler(async (request, response) => {
     const follower = request.appSession.follower;
-    // In case the user is not a follower, he gets no resonators
-    const followerResonators = follower ? await getResonators(follower.id) : [];
-    const activeResonators = followerResonators.filter(resonator => resonator.pop_email);
 
-    response.status(200);
-    response.json({ resonators: activeResonators });
+    if (follower)
+        response
+            .status(200)
+            .json({
+                resonators: await getFollowerResonators(follower)
+            })
+    else
+        response
+            .status(405)
+            .json({
+                status: "You must be a follower to have resonators"
+            })
 }));
+
+
+const getFollowerResonators = async follower =>
+    await resonators.findAll({
+        where: {
+            follower_id: follower.id,
+            pop_email: true
+        },
+        include: [
+            leaders,
+            resonator_attachments,
+            {
+                model: resonator_questions,
+                include: [questions]
+            },
+            {
+                model: sent_resonators,
+                order: [['created_at', 'DESC']],
+                limit: 1,
+                include: [resonator_answers]
+            }
+        ]
+    })
