10a5840335c0676559afdc89af957cfdd739e180
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-07-23 18:06:19 +0300

    convert data to relevant format and add question-answer aggregation

--- src/api/routes/follower.js
+++ src/api/routes/follower.js
@@ -1,9 +1,10 @@
-import express from '../express';
+import { Model } from 'sequelize';
 
+import app from '../express';
 import routeHandler from '../routeHandler';
+import Follower from '../../domain/entities/follower';
 import {
     resonators,
-    questions,
     resonator_attachments,
     resonator_questions,
     sent_resonators,
@@ -11,14 +12,14 @@ import {
 } from '../../db/sequelize/models'
 
 
-express.get('/api/follower/resonators', routeHandler(async (request, response) => {
+app.get('/api/follower/resonators', routeHandler(async (request, response) => {
     const follower = request.appSession.follower;
 
     if (follower)
         response
             .status(200)
             .json({
-                resonators: await getFollowerResonators(follower)
+                resonators: (await getFollowerResonators(follower)).map(formatResonatorPreview)
             })
     else
         response
@@ -29,24 +30,52 @@ express.get('/api/follower/resonators', routeHandler(async (request, response) =
 }));
 
 
+/**
+ * Queries the DB for minimal data relevant for previewing the resonators belonging to a certain follower.
+ * 
+ * @param {Follower} follower - the follower for whom to find their resonators
+ * @returns {Promise<Array<Model>>} - the resonators belonging to the given follower user
+ */
 const getFollowerResonators = async follower =>
     await resonators.findAll({
         where: {
             follower_id: follower.id,
-            pop_email: true  // filter out inactive resonators
+            pop_email: true  // filter out inactive resonators for followers
         },
         include: [
             resonator_attachments,
+            resonator_questions,  // required for counting total questions in resonators
             {
-                model: resonator_questions,
-                include: [questions]
-            },
-            {
-                // required for checking for unanswered questions
+                // required for checking for counting answered questions for last resonator instance
                 model: sent_resonators,
+                include: [resonator_answers],
                 order: [['created_at', 'DESC']],
-                limit: 1,
-                include: [resonator_answers]
+                limit: 1
             }
         ]
     })
+
+
+/**
+ * Converts a resonator's data to a format suitable for a minimal follower preview.
+ * 
+ * Note: this should have been possible to achieve in the above query itself,
+ *       but proved to be too complicated to be worth it.
+ * 
+ * @param {Model} resonator - a resonator as queried from the DB
+ * @returns {Object}
+ */
+const formatResonatorPreview = resonator => ({
+    id: resonator.id,
+    title: resonator.title,
+    content: resonator.content,
+    popTime: resonator.pop_time,
+    popDays: resonator.repeat_days,
+    lastPopTime: resonator.last_pop_time,
+    isOneTime: resonator.one_off,
+    attachments: resonator.resonator_attachments,
+    questions: {
+        total: resonator.resonator_questions.length,
+        answered: resonator.sent_resonators[0].resonator_answers.length
+    }
+})
