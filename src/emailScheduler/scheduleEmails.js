a96139506e48acab0f1c1120f9ac92a4a0393ef9
Ofek Sharon <Ofekinho@gmail.com>
2020-08-12 22:12:27 +0300

    Added check if follower group is frozen before sending resonator

--- src/emailScheduler/scheduleEmails.js
+++ src/emailScheduler/scheduleEmails.js
@@ -1,26 +1,33 @@
 import _ from 'lodash';
 import fetchPendingResonators from './fetchPendingResonators';
-import {resonators, resonator_attachments, followers, leaders, users, resonator_questions, questions, answers, sent_resonators} from '../db/sequelize/models';
+import { resonators, resonator_attachments, followers, follower_groups, leaders, users, resonator_questions, questions, answers, sent_resonators } from '../db/sequelize/models';
 import * as dbToDomain from '../db/dbToDomain';
 import renderResonatorEmail from '../emailRenderer/index';
 import sendResonatorEmail from './sendResonatorEmail';
 import cfg from '../cfg';
 import uuid from 'uuid/v4';
-import {emailSchedulerLogger as log} from '../infra/log';
+import { emailSchedulerLogger as log } from '../infra/log';
 
 export default async function scheduleEmails(getNow) {
     log.info('[emailScheduler] fetching pending resonators');
     const resonatorIds = await fetchPendingResonators(getNow);
+    console.log({resonatorIds});
 
     if (resonatorIds.length > 0) {
         const resonatorData = await getResonatorsData(resonatorIds);
 
-        const emailPromises = _(resonatorData).map(({
+        const emailPromises = _(resonatorData).map(async ({
             resonator,
             followerUser,
             follower,
             leaderUser
         }) => {
+            if (resonator.parent_resonator_id) {
+                const followerGroup = await getParentFollowerGroup(resonator.parent_resonator_id);
+                if ((followerGroup && followerGroup.frozen)) {
+                    return Promise.resolve();
+                }
+            }
             if (followerUser.unsubscribed) {
                 return Promise.resolve();
             }
@@ -111,12 +118,12 @@ function sendEmail({resonator, followerUser, leaderUser}) {
 }
 
 function recordSentResonator({ id, ttl_policy }) {
-    const expiryDate = new Date();
-    expiryDate.setTime(expiryDate.getTime() + (ttl_policy * 60 * 60 * 1000));
+    // const expiryDate = new Date();
+    // expiryDate.setTime(expiryDate.getTime() + (ttl_policy * 60 * 60 * 1000));
     return sent_resonators.create({
         id: uuid(),
         resonator_id: id,
-        expiry_date: expiryDate,
+        // expiry_date: expiryDate,
         failed: false
     });
 }
@@ -135,8 +142,24 @@ function disableResonatorForSendOneOff(resonatorId) {
     return resonators.update({
         pop_email: false
     }, {
-            where: {
-                id: resonatorId
+        where: {
+            id: resonatorId
+        }
+    });
+}
+
+function getParentFollowerGroup(parentResonatorId) {
+    console.log({parentResonatorId});
+    return resonators.findOne({
+        where: {
+            id: parentResonatorId
+        },
+        include: [
+            {
+                model: follower_groups,
             }
-        });
+        ]
+    }).then((row) =>
+        row.follower_group && dbToDomain.toFollowerGroup(row.follower_group)
+    );
 }
