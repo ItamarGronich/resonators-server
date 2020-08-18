6c9177adcbc5f703010fc8992e6632749f8520d7
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-08-18 13:30:11 +0300

    added theme color and manifest to html

--- src/api/renderClient.js
+++ src/api/renderClient.js
@@ -3,11 +3,15 @@ import {getLatestAssetLink} from '../application/versionableAssets';
 
 export default async function renderClient(request, response, pageData) {
     let clientVersion = await getLatestAssetLink('resonators-client');
+    let manifestVersion = await getLatestAssetLink('manifest');
 
     if (process.env.ENV === 'dev')
         clientVersion = {
             link: 'http://localhost:8000/assets/app.js'
         };
+        manifestVersion = {
+            link: 'http://localhost:8000/assets/manifest.webmanifest'
+        }
 
     const serverVersion = await readVersion();
     const serverVersionTxt = JSON.stringify(serverVersion);
@@ -19,6 +23,7 @@ export default async function renderClient(request, response, pageData) {
         clientVersion: clientVersionTxt,
         serverVersion: serverVersionTxt,
         scriptUrl: clientVersion.link,
+        manifestUrl: manifestVersion.link,
         pageData: pageData && JSON.stringify(pageData)
     });
 }
