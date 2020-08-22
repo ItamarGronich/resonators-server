f4b70d1c8e7972e785420b4931e2fe2de79a66d1
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-08-22 20:04:50 +0300

    removed page serving logic

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -5,7 +5,6 @@ import './leader_followerGroups';
 import './reminders';
 import './groupReminders';
 import './renderResonator';
-import './pages.js';
 import './stats.js';
 import './users';
 import './user_password_resets';
