57e44f3de522dd4934b1ee93bfa8a739ecdcb4c4
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-09-23 14:41:32 +0300

    fixed dropped spread method, and beautified code

--- src/emailScheduler/fetchPendingResonators.js
+++ src/emailScheduler/fetchPendingResonators.js
@@ -1,21 +1,35 @@
-import db from '../db/sequelize/dbConnection';
+import { QueryTypes } from "sequelize";
 
-export default function fetchPendingResonators(getNow = (() => new Date())) {
-    const now = "'" + getNow().toJSON() + "'";
+import db from "../db/sequelize/dbConnection";
 
-    const sql = `select r.id from resonators r ` +
-        `left join followers f on f.id = r.follower_id ` +
-        `left join users fu on fu.id = f.user_id ` +
-        `left join leaders l on l.id = r.leader_id ` +
-        `left join users lu on lu.id = l.user_id ` +
-        `where (last_pop_time is not null or ${now}::timestamp > pop_time::timestamp) and ` +
-        `(last_pop_time is null or date(${now}) > date(coalesce(last_pop_time, '1970-01-01'))) and ` +
-        `${now}::timestamp::time >= pop_time::time and ` +
-        `position(extract(dow from ${now}::timestamp)::char in repeat_days) > 0 and ` +
-        `pop_email = true and ` +
-        `fu.id is not null and ` +
-        `lu.id is not null and ` +
-        `r.follower_group_id is null`;
+function buildQuery(timestamp) {
+    return `SELECT r.id FROM resonators r
+         LEFT JOIN followers f ON f.id = r.follower_id
+         LEFT JOIN users fu ON fu.id = f.user_id
+         LEFT JOIN leaders l ON l.id = r.leader_id
+         LEFT JOIN users lu ON lu.id = l.user_id
+             WHERE (last_pop_time IS NOT NULL OR ${timestamp}::timestamp > pop_time::timestamp)
+               AND (last_pop_time IS NULL OR date(${timestamp}) > date(coalesce(last_pop_time, '1970-01-01')))
+               AND ${timestamp}::timestamp::time >= pop_time::time
+               AND position(extract(dow from ${timestamp}::timestamp)::char IN repeat_days) > 0
+               AND pop_email = TRUE
+               AND fu.id IS NOT NULL
+               AND lu.id IS NOT NULL
+               AND r.follower_group_id IS NULL`;
+}
 
-    return db.query(sql).spread(rows => rows.map(r => r.id));
+function formatTimestamp(datetime) {
+    return `'${datetime.toJSON()}'`;
+}
+
+function executeRawSql(sql) {
+    return db.query(sql, { type: QueryTypes.SELECT });
+}
+
+function extractIds(queryResults) {
+    return queryResults.map((result) => result.id);
+}
+
+export default function fetchPendingResonators(asOf) {
+    return executeRawSql(buildQuery(formatTimestamp(asOf || new Date()))).then(extractIds);
 }
