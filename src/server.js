cc9659acf9b1c058e89e6f08e93be9147a56afad
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-09-23 14:16:01 +0300

    updated babel setup and moved index.js into src

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import api from "./api";
+import cfg from "./cfg";
+import initInfra from "./infra";
+import emailSchedulingJob from "./emailScheduler";
+import { setVapidKeys } from "./emailScheduler/push";
+// import syncCalendarsJob from "./calendars/calendarsSync";
+
+function startServer() {
+    api.listen(cfg.port, "0.0.0.0", () => {
+        console.log(`API running on port ${cfg.port} with ${process.env.ENV.toUpperCase()} environment`);
+    });
+}
+
+function startJobs() {
+    if (cfg.emailSchedulerOn) emailSchedulingJob.start();
+    // syncCalendarsJob.start();
+}
+
+initInfra();
+setVapidKeys();
+startJobs();
+startServer();
