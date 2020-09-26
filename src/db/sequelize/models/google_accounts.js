bc084473f17f285b4ce00e50fc04a5cfede19ed0
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-09-26 23:52:42 +0300

    F**********CKING UNDERSCORED

--- src/db/sequelize/models/google_accounts.js
+++ src/db/sequelize/models/google_accounts.js
@@ -1,11 +1,15 @@
 "use strict";
 module.exports = (sequelize, DataTypes) =>
-    sequelize.define("google_accounts", {
-        id: { type: DataTypes.UUID, primaryKey: true },
-        id_token: DataTypes.STRING,
-        access_token: DataTypes.STRING,
-        refresh_token: DataTypes.STRING,
-        access_token_expiry_date: DataTypes.DATE,
-        user_id: DataTypes.UUID,
-        google_email: DataTypes.STRING,
-    });
+    sequelize.define(
+        "google_accounts",
+        {
+            id: { type: DataTypes.UUID, primaryKey: true },
+            id_token: DataTypes.STRING,
+            access_token: DataTypes.STRING,
+            refresh_token: DataTypes.STRING,
+            access_token_expiry_date: DataTypes.DATE,
+            user_id: DataTypes.UUID,
+            google_email: DataTypes.STRING,
+        },
+        { underscored: true }
+    );
