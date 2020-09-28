1ed8cbd8af82b5dd3ab009f283b477ae5a5ea9cf
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-09-28 15:33:52 +0300

    removed log wrapper which basically did nothing

--- src/logging/logger.js
+++ src/logging/logger.js
@@ -3,7 +3,6 @@ import winston from "winston";
 import { kebabCase, isEmpty } from "lodash";
 
 import config from "../cfg";
-import logWrapper from "./logWrapper";
 
 function formatMetadata(metadata) {
     return isEmpty(metadata)
@@ -18,25 +17,22 @@ function formatter({ timestamp, level, label, message, metadata }) {
 }
 
 export function createLogger(name) {
-    return logWrapper(
-        winston.createLogger({
-            format: winston.format.combine(
-                winston.format.errors({ stack: true }),
-                winston.format.metadata(),
-                winston.format.timestamp(),
-                winston.format.label({ label: name }),
-                winston.format.printf(formatter)
-            ),
-            transports: [
-                new winston.transports.File({
-                    filename: path.join(config.logging.directory, kebabCase(name), kebabCase(name)),
-                    maxsize: config.logging.maxFileSize,
-                    maxFiles: config.logging.maxFiles,
-                    tailable: true,
-                }),
-                new winston.transports.Console(),
-            ],
-        }),
-        true
-    );
+    return winston.createLogger({
+        format: winston.format.combine(
+            winston.format.errors({ stack: true }),
+            winston.format.metadata(),
+            winston.format.timestamp(),
+            winston.format.label({ label: name }),
+            winston.format.printf(formatter)
+        ),
+        transports: [
+            new winston.transports.File({
+                filename: path.join(config.logging.directory, kebabCase(name), kebabCase(name)),
+                maxsize: config.logging.maxFileSize,
+                maxFiles: config.logging.maxFiles,
+                tailable: true,
+            }),
+            new winston.transports.Console(),
+        ],
+    });
 }
