94f603b1e60ccfdb79f559d065e7dd6065b2d21d
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-09-30 18:05:08 +0300

    added documentation

--- src/logging/format.js
+++ src/logging/format.js
@@ -2,25 +2,42 @@ import { EOL } from "os";
 import { isEmpty } from "lodash";
 import indent from "indent-string";
 
+/**
+ * Formats the core part of a log message.
+ */
 function formatMessage({ timestamp, level, label, message }) {
     return `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`;
 }
 
+/**
+ * Serializes any metadata value in a readable fashion.
+ */
 function serialize(value) {
     return JSON.stringify(value, null, 4);
 }
 
+/**
+ * Formats all extra meta-details attached to a log message.
+ */
 function formatExtraDetails(details) {
     return Object.entries(details)
         .map(([name, value]) => `${name}: ${serialize(value)}`)
         .join(EOL);
 }
 
+/**
+ * Formats any metadata attached to the log message.
+ * 
+ * Error stacks are separated and printed as-is before other details.
+ */
 function formatMetadata({ stack, ...extra }) {
     const meta = isEmpty(extra) ? "" : formatExtraDetails(extra);
     return stack && meta ? [stack, meta].join(EOL) : stack || meta;
 }
 
+/**
+ * Formats a log info object to a printable string.
+ */
 export default function formatLog(info) {
     const message = formatMessage(info);
     const metadata = formatMetadata(info.metadata);
--- src/logging/logger.js
+++ src/logging/logger.js
@@ -7,18 +7,25 @@ import formatLog from "./format";
 
 const TIMESTAMP_FORMAT = "YYYY-MM-DD HH:mm:ss.SSS";
 
+/**
+ * Creates a new logger instance to be used by a component of the system.
+ *
+ * @param {String} name - the name of the logger.
+ *                        This will be used for labeling log messages and naming log files.
+ */
 export function createLogger(name) {
     const logger = winston.createLogger({
         level: "debug",
         format: winston.format.combine(
-            winston.format.errors({ stack: true }),
-            winston.format.metadata(),
-            winston.format.label({ label: name }),
-            winston.format.timestamp({ format: TIMESTAMP_FORMAT }),
-            winston.format.printf(formatLog)
+            winston.format.errors({ stack: true }), // extract error stacks from error objects
+            winston.format.metadata(), // support extra details appended to logs (useful for debugging)
+            winston.format.label({ label: name }), // label log messages by their name
+            winston.format.timestamp({ format: TIMESTAMP_FORMAT }), // add log timestamps
+            winston.format.printf(formatLog) // finalize and format into a string
         ),
         transports: [
             new winston.transports.File({
+                // separating logs from different loggers to separate sub-directories
                 filename: path.join(config.logging.directory, kebabCase(name), kebabCase(name)),
                 maxsize: config.logging.maxFileSize,
                 maxFiles: config.logging.maxFiles,
@@ -27,6 +34,7 @@ export function createLogger(name) {
         ],
     });
 
+    // We don't need console logging in production since nobody is looking at it...
     if (process.env.ENV !== "production") {
         logger.add(new winston.transports.Console());
     }
