929d744d6271d049028a1143afc5845592d8a193
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2020-09-30 17:55:12 +0300

    make sure the stack comes before extra metadata, and increase indentation

--- src/logging/format.js
+++ src/logging/format.js
@@ -6,24 +6,23 @@ function formatMessage({ timestamp, level, label, message }) {
     return `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`;
 }
 
-function stringify(value) {
-    return JSON.stringify(value, null, 2);
+function serialize(value) {
+    return JSON.stringify(value, null, 4);
 }
 
-function formatMetaValue([name, value]) {
-    if (name === "stack") {
-        // print error stacks as-is
-        return value;
-    }
-    return `${name}: ${stringify(value)}`;
+function formatExtraDetails(details) {
+    return Object.entries(details)
+        .map(([name, value]) => `${name}: ${serialize(value)}`)
+        .join(EOL);
 }
 
-function formatMetadata(metadata) {
-    return isEmpty(metadata) ? "" : Object.entries(metadata).map(formatMetaValue).join(EOL);
+function formatMetadata({ stack, ...extra }) {
+    const meta = isEmpty(extra) ? "" : formatExtraDetails(extra);
+    return stack && meta ? [stack, meta].join(EOL) : stack || meta;
 }
 
 export default function formatLog(info) {
     const message = formatMessage(info);
     const metadata = formatMetadata(info.metadata);
-    return metadata ? [message, indent(metadata, 2)].join(EOL) : message;
+    return metadata ? [message, indent(metadata, 4)].join(EOL) : message;
 }
