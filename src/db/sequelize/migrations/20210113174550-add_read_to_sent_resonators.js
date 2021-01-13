d2066f75d64921dbcab0e08d4ac0e35b959ee887
Raverside <r4verside@gmail.com>
2021-01-13 21:03:36 +0200

    added read column to sent_resonators

--- src/db/sequelize/migrations/20210113174550-add_read_to_sent_resonators.js
+++ src/db/sequelize/migrations/20210113174550-add_read_to_sent_resonators.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('sent_resonators', 'read', { type: Sequelize.BOOLEAN });
+    return queryInterface.addColumn('sent_resonators', 'read', { type: Sequelize.BOOLEAN, defaultValue: false });
   },
 
   down: async (queryInterface, Sequelize) => {
