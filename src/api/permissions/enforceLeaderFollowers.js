33190f6b6086e8024a909bca546da640a15d40c2
Raverside <r4verside@gmail.com>
2021-03-27 01:29:31 +0200

    allow viewing system resonators for everyone

--- src/api/permissions/enforceLeaderFollowers.js
+++ src/api/permissions/enforceLeaderFollowers.js
@@ -4,7 +4,7 @@ export default async function enforceLeaderFollowers(request, response) {
     const leader = request.appSession.leader || {};
     const followerId = request.params.followerId;
     const follower = await followerRepository.findByPk(followerId);
-    if (follower && follower.leader_id !== leader.id) {
+    if (!leader.admin_permissions && follower && !follower.is_system && follower.leader_id !== leader.id) {
         response.status(403);
         response.json({ status: 'leader is not permitted to view or edit the given follower.'});
         return false;
